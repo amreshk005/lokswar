17c87966c33dab02acccee620a1aa7c38ea2897e
amreshk005 <amreshk005@gmail.com>
2019-03-27 17:38:33 +0530

    static media change

--- lokswar/lokswar/settings.py
+++ lokswar/lokswar/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+ 
             ],
         },
     },
@@ -132,6 +134,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static','media_root/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static',"media_root/")
 
 
